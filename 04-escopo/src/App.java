ed6bfad40d925f012f692af2ef3c3f24229f30a2
Matheus <mt121207@gmail.com>
2024-04-29 19:56:24 -0300

    Adicionando alguns testes feito com class Conta e utilizando na main App.

--- 04-escopo/src/App.java
+++ 04-escopo/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Conta conta = new Conta();
+        conta.sacar(6.00);
+        conta.imprimirSaldo();
     }
 }
