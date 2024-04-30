42806ad9d88dd0fcb8f943c8500b025525867ee5
Matheus <mt121207@gmail.com>
2024-04-30 13:50:17 -0300

    Ajustando classe ResultadoEscolar com condições encadeadas.

--- 08-controle-de-fluxo/src/ResultadoEscolar.java
+++ 08-controle-de-fluxo/src/ResultadoEscolar.java
@@ -4,9 +4,11 @@ public class ResultadoEscolar {
         int nota = 8;
 
         if (nota >= 7)
-            System.out.println("Aprovado");
+            System.out.println("Aprovado!");
 
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação!");
         else
-            System.out.println("Reprovado");
+            System.out.println("Reprovado!");
     }
 }
