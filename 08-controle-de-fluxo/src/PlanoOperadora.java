bdbf17932ea03a55f6c95b221ecb9d137180502f
Matheus <mt121207@gmail.com>
2024-04-30 16:29:10 -0300

    Utilização de if/else para switch case e utilização do próprio switch case

---
+++ 08-controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,31 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        // // VARIÁVEL PARA AMBOS OS CASOS. ↓
+        // String plano = "M"; // M / T
+
+        // // MODO CONDICIONAL CONVENCIONAL. ↓
+        // if (plano == "B") {
+        // System.out.println("100 minutos de ligação");
+        // } else if (plano == "M") {
+        // System.out.println("100 minutos de ligação");
+        // System.out.println("Whats e Intagram grátis");
+        // } else if (plano == "T") {
+        // System.out.println("100 minutos de ligação");
+        // System.out.println("Whats e Intagram grátis");
+        // System.out.println("5Gb Youtube");
+        // }
+
+        // // MODO CONDICIONAL SWITCH/CASE. ↓
+        // switch (plano) {
+        // case "T": {
+        // System.out.println("5Gb Youtube");
+        // }
+        // case "M": {
+        // System.out.println("Whats e Intagram grátis");
+        // }
+        // case "B": {
+        // System.out.println("100 minutos de ligação");
+        // }
+        // }
+    }
+}
---
+++ 08-controle-de-fluxo/src/SistemaMedida.java
@@ -0,0 +1,35 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        // String sigla = "M";
+        // // ------------ SWITCH CASE UTILIZANDO IF/ELSE. ↓
+
+        // if (sigla == "P")
+        // System.out.println("PEQUENO");
+        // else if (sigla == "M")
+        // System.out.println("MEDIO");
+        // else if (sigla == "G")
+        // System.out.println("GRANDE");
+        // else
+        // System.out.println("INDEFINIDO");
+
+        // // ------------ SWITCH CASE. ↓
+
+        // switch (sigla) {
+        // case "P": {
+        // System.out.println("PEQUENO");
+        // break;
+        // }
+        // case "M": {
+        // System.out.println("MEDIO");
+        // break;
+        // }
+        // case "G": {
+        // System.out.println("GRANDE");
+        // break;
+        // }
+        // default:
+        // System.out.println("INDEFINIDO");
+        // }
+
+    }
+}
