93ae2a9e487258475b2dc5febfbc280820db5e70
Matheus <mt121207@gmail.com>
2024-04-30 16:09:21 -0300

    Ajustando código na classe ResultadoEscolar para exemplos de Condição ternária

--- 08-controle-de-fluxo/src/ResultadoEscolar.java
+++ 08-controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,14 +1,28 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
+        // // -------------- CÓDIGO COM CONDIÇÃO ENCADEADA. ↓
+        // int nota = 8;
 
-        int nota = 8;
+        // if (nota >= 7)
+        // System.out.println("Aprovado!");
 
-        if (nota >= 7)
-            System.out.println("Aprovado!");
+        // else if (nota >= 5 && nota < 7)
+        // System.out.println("Recuperação!");
+        // else
+        // System.out.println("Reprovado!");
+        // }
 
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Recuperação!");
-        else
-            System.out.println("Reprovado!");
+        // // -------------- CONDIÇÃO TERNÁRIA. ↓
+        // int nota = 7;
+        // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        // System.out.println(resultado);
+
+        // // -------------- CONDIÇÃO TERNÁRIA SIMULANDO ELSE IF.
+        // int nota2 = 6;
+        // String resultado2 = nota2 >= 7 ? "Aprovado" : nota2 >= 5 && nota2 < 7 ?
+        // "Recuperação" : "Reprovado";
+        // System.out.println(resultado2);
+
     }
 }
