3c2c522e12cdbc81516a34f98b7ffba7f0ad906a
Matheus <mt121207@gmail.com>
2024-05-01 10:52:04 -0300

    Exemplo de criação de excecao customizada.

---
+++ 10-tratamento-de-excessoes/src/ExemploExcecao.java
@@ -0,0 +1,20 @@
+// import java.text.NumberFormat;
+// import java.text.ParseException;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        // // EXEMPLO DE EXCECAO NAO CHECADA. ↓
+        // Number valor = Double.valueOf("a1.75");
+
+        // System.out.println(valor);
+
+        // // EXEMPLO DE EXCECAO CHECADA. ↓
+        // Number valor;
+        // try {
+        // valor = NumberFormat.getInstance().parse("a1.75");
+        // System.out.println(valor);
+        // } catch (ParseException e) {
+        // e.printStackTrace();
+        // }
+    }
+}
